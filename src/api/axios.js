b36128fa834a7b2dc1436659675987ba0a12cb1c
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-05-17 22:54:07 -0400

    Arreglando ruta de axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL;
 const api=axios.create({
-    baseURL:'http://127.0.0.1:8000/api',
+    baseURL:`${apiUrl}`,
     timeout: 10000,
     headers:{
             'Accept':'application/json',
